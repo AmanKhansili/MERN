229ec7895d5a6c5a4aef606db7e41562b133afe3
Aman Khansili <amankhansilee@gmail.com>
2024-12-10 00:09:55 +0530

    Profile Model

--- Project01/models/profile.model.js
+++ Project01/models/profile.model.js
@@ -1,3 +1,117 @@
-import mongoose from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
+import { User } from "./user.model.js";
 
-const profileSchema = new mongoose.Schema
+const profileSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: User,
+    },
+    company: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: String,
+    },
+    company: {
+      type: [String],
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    githubusername: {
+      type: String,
+    },
+    experience: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        company: {
+          type: String,
+          required: true,
+        },
+        location: {
+          type: String,
+        },
+        from: {
+          type: Date,
+          required: true,
+        },
+        to: {
+          type: Date,
+        },
+        current: {
+          type: Boolean,
+          default: false,
+        },
+        description: {
+          type: String,
+        },
+      },
+    ],
+    education: [
+      {
+        school: {
+          type: String,
+          required: true,
+        },
+        degree: {
+          type: String,
+          required: true,
+        },
+        fieldofstudy: {
+          type: String,
+          required: true,
+        },
+        from: {
+          type: Date,
+          required: true,
+        },
+        to: {
+          type: Date,
+        },
+        current: {
+          type: Boolean,
+          default: false,
+        },
+        description: {
+          type: String,
+        },
+      },
+    ],
+    social: {
+      youtube: {
+        typr: String,
+      },
+      twitter: {
+        typr: String,
+      },
+      facebook: {
+        typr: String,
+      },
+      linkedin: {
+        typr: String,
+      },
+      instagram: {
+        typr: String,
+      },
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Profile = model("Profile", profileSchema);
