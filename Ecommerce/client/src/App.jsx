ae6c2343e5032287183416bdd68dd23fe1d62bf4
Aman Khansili <amankhansilee@gmail.com>
2024-12-13 00:34:19 +0530

    creating a navbar

--- Ecommerce/client/src/App.jsx
+++ Ecommerce/client/src/App.jsx
@@ -1,12 +1,36 @@
 import { useState } from "react";
-import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello welcome</h1>
+      <ThemeProvider theme={lightTheme}>
+        <BrowserRouter>
+          <Container>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Container>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
---
+++ Ecommerce/client/src/pages/ShopingListing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShopingListing = () => {
+  return <div>ShopingListing</div>;
+};
+
+export default ShopingListing;
