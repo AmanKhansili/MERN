a821ce712123b1cb240bae326c8a859c072387d9
Aman Khansili <amankhansilee@gmail.com>
2024-12-26 20:38:50 +0530

    adding all the User Routes

--- Ecommerce/server/routes/user.route.js
+++ Ecommerce/server/routes/user.route.js
@@ -1,9 +1,34 @@
 import express from "express";
-import { UserLogin, UserRegister } from "../controller/User.js";
+import {
+  addToCart,
+  addToFavorites,
+  getAllcatItems,
+  getAllOrders,
+  getUserFavorites,
+  placeOrder,
+  removeFromCart,
+  removeFromFavorites,
+  UserLogin,
+  UserRegister,
+} from "../controller/User.js";
 
 const router = express.Router();
 
 router.post("/signup", UserRegister);
 router.post("/signin", UserLogin);
+
+//cart
+router.get("/cart", getAllcatItems);
+router.post("/cart", addToCart);
+router.patch("/cart", removeFromCart);
+
+//order
+router.get("/order", getAllOrders);
+router.post("/order", placeOrder);
+
+//favourites
+router.get("/favorite", getUserFavorites);
+router.post("/favorite", addToFavorites);
+router.patch("/favorite", removeFromFavorites);
 
 export default router;
