e500e09e486b30c7308f02115843fd858bbf5431
Aman Khansili <amankhansilee@gmail.com>
2025-01-09 23:59:14 +0530

    using redux in client

---
+++ Ecommerce/client/src/api/index.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:4000/api/",
+});
+
+export const UserSignUp = async (data) => await API.post("/user/signup", data);
+export const UserSignIn = async (data) => await API.post("/user/signin", data);
+
+//products
+export const getAllProducts = async (filter) =>
+  await API.get(`/products?${filter}`);
+export const getProductDetails = async (id) => await API.get(`/products/${id}`);
+
+//Cart
+
+export const getCart = async (token) =>
+  await API.get("/user/cart", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const addToCart = async (token, data) =>
+  await API.post(`/user/cart/`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const deleteFromCart = async (token, data) =>
+  await API.patch(`/user/cart`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+//Favourites
+
+export const getFavourite = async (token) =>
+  await API.get("/user/favorite", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const addToFavourite = async (token, data) =>
+  await API.post(`/user/favorite/`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const deleteFromFavourite = async (token, data) =>
+  await API.patch(`/user/favorite`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+//Orders
+
+export const getOrder = async (token, data) =>
+  await API.get(`/user/order`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+export const placeOrder = async (token, data) =>
+  await API.post(`/user/order`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
